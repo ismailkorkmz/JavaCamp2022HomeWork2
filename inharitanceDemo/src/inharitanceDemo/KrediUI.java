1ab5a8a64aa4840d272f22b590cdb13c4fd5d377
hasankorogluismailkorkmz <hilal_ay_1907@windowslive.com>
2022-09-28 17:48:09 +0300

    Hafta 2 Ödev

---
+++ inharitanceDemo/src/inharitanceDemo/KrediUI.java
@@ -0,0 +1,8 @@
+package inharitanceDemo;
+
+public class KrediUI {
+	public void KrediHesapla(BaseKrediManager baseKrediManager) {
+		baseKrediManager.Hesapla();
+	}
+
+}
---
+++ inharitanceDemo/src/inharitanceDemo/Main.java
@@ -0,0 +1,16 @@
+package inharitanceDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		OgretmenKrediManager ogretmenKrediManager =new OgretmenKrediManager();
+		ogretmenKrediManager.Hesapla();
+		
+		KrediUI krediUI = new KrediUI();
+		krediUI.KrediHesapla(new OgretmenKrediManager());
+		krediUI.KrediHesapla(new TarimKrediManager());
+		krediUI.KrediHesapla(new AskerKrediManager());
+
+	}
+
+}
